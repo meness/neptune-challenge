d1963a2a7ac4d6f2345f542cbb912c7bd02e9510
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-23 11:35:50 +0330

    ui: better manage center wrapper height

--- src/components/wrappers/CenterWrapper.tsx
+++ src/components/wrappers/CenterWrapper.tsx
@@ -1,11 +1,11 @@
 import { Box } from '@mui/material';
 import { styled } from '@mui/system';
 
-const CenterWrapper = styled(Box)({
+const CenterWrapper = styled(Box)(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  height: '100vh',
-});
+  height: `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
+}));
 
 export default CenterWrapper;
