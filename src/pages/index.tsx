d0ce614b6975c13755c37bd1c344d93cb012a864
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-23 22:02:16 +0330

    fix: better make card center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ import {
   Select,
   TextField,
 } from '@mui/material';
-import { CenterWrapper } from '@wrappers';
 import Head from 'next/head';
 
 const dummyPairs = [
@@ -30,66 +29,74 @@ const Converter = ({}: ConverterProps) => {
       <Head>
         <title>Home</title>
       </Head>
-      <CenterWrapper>
-        <Card>
-          <CardHeader title="Crypto Converter" subheader="Inputs will be updated instantly." />
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <InputLabel id="pair">Pair</InputLabel>
-                  <Select labelId="pair" label="Pair" value={pair} onChange={(e) => setPair(e.target.value)}>
-                    {dummyPairs.map((dummyPair) => (
-                      <MenuItem value={dummyPair.id} selected={dummyPair.id === selectedDummyPair} key={dummyPair.id}>
-                        {dummyPair.spend}/{dummyPair.receive}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+      <Grid
+        container
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: (theme) => `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
+        }}>
+        <Grid item xs={12} sm={8} md={7} lg={6}>
+          <Card>
+            <CardHeader title="Crypto Converter" subheader="Inputs will be updated instantly." />
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <InputLabel id="pair">Pair</InputLabel>
+                    <Select labelId="pair" label="Pair" value={pair} onChange={(e) => setPair(e.target.value)}>
+                      {dummyPairs.map((dummyPair) => (
+                        <MenuItem value={dummyPair.id} selected={dummyPair.id === selectedDummyPair} key={dummyPair.id}>
+                          {dummyPair.spend}/{dummyPair.receive}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    type="number"
+                    label="Spend"
+                    variant="outlined"
+                    placeholder="0"
+                    name="spend"
+                    fullWidth
+                    value={spend}
+                    onChange={(e) => setSpendValue(parseFloat(e.target.value))}
+                    inputProps={{
+                      min: 0,
+                      inputMode: 'numeric',
+                      pattern: '[0-9.]*',
+                      step: '0.01',
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12} sx={{ textAlign: 'center' }}>
+                  <AutorenewOutlined />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    type="number"
+                    label="Receive"
+                    placeholder="0"
+                    variant="outlined"
+                    name="receive"
+                    fullWidth
+                    value={receive}
+                    onChange={(e) => setReceiveValue(parseFloat(e.target.value))}
+                    inputProps={{
+                      min: 0,
+                      inputMode: 'numeric',
+                      pattern: '[0-9.]*',
+                      step: '0.01',
+                    }}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  type="number"
-                  label="Spend"
-                  variant="outlined"
-                  placeholder="0"
-                  name="spend"
-                  fullWidth
-                  value={spend}
-                  onChange={(e) => setSpendValue(parseFloat(e.target.value))}
-                  inputProps={{
-                    min: 0,
-                    inputMode: 'numeric',
-                    pattern: '[0-9.]*',
-                    step: '0.01',
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} sx={{ textAlign: 'center' }}>
-                <AutorenewOutlined />
-              </Grid>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  type="number"
-                  label="Receive"
-                  placeholder="0"
-                  variant="outlined"
-                  name="receive"
-                  fullWidth
-                  value={receive}
-                  onChange={(e) => setReceiveValue(parseFloat(e.target.value))}
-                  inputProps={{
-                    min: 0,
-                    inputMode: 'numeric',
-                    pattern: '[0-9.]*',
-                    step: '0.01',
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </CenterWrapper>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 };
