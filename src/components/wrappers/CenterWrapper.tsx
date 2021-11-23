8178adf8b1a8fb99e6d79bc15a5e96f9d04720be
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-23 10:30:11 +0330

    feat: new center wrapper component

---
+++ src/components/wrappers/CenterWrapper.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+const CenterWrapper = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh',
+});
+
+export default CenterWrapper;
